5ec4e4f0e0cfa850244b8388a446fcb3a3310b11
hlwhl <h2113191@gmail.com>
2018-05-14 10:41:18 +0100

    修改关于界面

--- app/src/main/java/com/soton/mobiledev/employeedirectory/activities/AboutActivity.java
+++ app/src/main/java/com/soton/mobiledev/employeedirectory/activities/AboutActivity.java
@@ -1,5 +1,7 @@
 package com.soton.mobiledev.employeedirectory.activities;
 
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,24 +10,45 @@ import android.widget.TextView;
 import com.soton.mobiledev.employeedirectory.R;
 
 public class AboutActivity extends AppCompatActivity {
+    private boolean state=true;
+    TextView tvAuthor;
+
+    Handler handler=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            updateUI();
+            return false;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        TextView tvAuthor= (TextView) findViewById(R.id.tvAuthor);
-
-        tvAuthor.setText("Hegang Xu & Linwei Hao");
-//        boolean temp=true;
-//        while (true){
-//            if(temp=true){
-//                tvAuthor.setText("Linwei Hao & Hegang Xu");
-//                temp=false;
-//            }else{
-//
-//                temp=true;
-//            }
-//            SystemClock.sleep(1000);
-//        }
+        tvAuthor= (TextView) findViewById(R.id.tvAuthor);
+        Thread thread=new updateUI();
+        thread.run();
+    }
+    private void updateUI(){
+        if(state){
+            tvAuthor.setText("Hegang Xu & Linwei Hao");
+            state=false;
+        }else{
+            tvAuthor.setText("Linwei Hao & Hegang Xu");
+            state=true;
+        }
+    }
+    class updateUI extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            Message message=new Message();
+            message.what=1;
+            while(true){
+                handler.sendMessage(message);
+                SystemClock.sleep(1000);
+            }
+        }
     }
 }
+

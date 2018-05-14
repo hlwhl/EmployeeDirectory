0f706f38b9eb1ee723b082f4e0958dd55a417ab1
hlwhl <h2113191@gmail.com>
2018-05-14 11:08:50 +0100

    final version

--- app/src/main/java/com/soton/mobiledev/employeedirectory/activities/AboutActivity.java
+++ app/src/main/java/com/soton/mobiledev/employeedirectory/activities/AboutActivity.java
@@ -2,14 +2,19 @@ package com.soton.mobiledev.employeedirectory.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.soton.mobiledev.employeedirectory.R;
 
 public class AboutActivity extends AppCompatActivity {
+    TextView tvAuthor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        tvAuthor= (TextView) findViewById(R.id.tvAuthor);
     }
+
 }
